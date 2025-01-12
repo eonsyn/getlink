dd0e953df10af05a34a7ef1d0181a8c852b7bd26
Aryan Singh <aryan03aryansingh@gmail.com>
2025-01-12 21:49:21 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const axios = require("axios");
 const { scrapdata } = require("./scrappingLogic.js");
 const Port = 2300;
 const app = express();
 app.get("/", (req, res) => {
   scrapdata(res);
+});
+app.get("/home", (req, res) => {
+  axios
+    .get(
+      "https://scrape.abstractapi.com/v1/?api_key=df74a5f42c584c7eb5d3b193d05ad1d1&url=https://hubcloud.ink/drive/juj0bb807xgashi"
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 app.listen(Port, () => {
   console.log("server is running at 2300");
--- scrappingLogic.js
+++ scrappingLogic.js
@@ -2,49 +2,31 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 const scrapdata = async (res) => {
-  const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
-  try {
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+  // Navigate the page to a URL.
+  await page.goto("https://developer.chrome.com/");
 
-    // Set screen size.
-    await page.setViewport({ width: 1080, height: 1024 });
+  // Set screen size.
+  await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box.
-    await page
-      .locator(".devsite-search-field")
-      .fill("automate beyond recorder");
+  // Type into search box.
+  await page.locator(".devsite-search-field").fill("automate beyond recorder");
 
-    // Wait and click on first result.
-    await page.locator(".devsite-result-item-link").click();
+  // Wait and click on first result.
+  await page.locator(".devsite-result-item-link").click();
 
-    // Locate the full title with a unique string.
-    const textSelector = await page
-      .locator("text/Customize and automate")
-      .waitHandle();
-    const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+  // Locate the full title with a unique string.
+  const textSelector = await page
+    .locator("text/Customize and automate")
+    .waitHandle();
+  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
 
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
-  }
+  // Print the full title.
+  console.log('The title of this blog post is "%s".', fullTitle);
+
+  await browser.close();
+  res.send(fullTitle);
 };
 module.exports = { scrapdata };
